2be17e6ae25bcdc37a8f21cc1dc4b7fcfb0e436a
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-10-22 12:34:32 -0300

    nodemon e CRUD

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ const server = express();
 // Route params = /users/1
 // Request body = { "name": Rafael, "email": "rafael.lindoso@ageof3d.com" }
 
+// CRUD - Create, Read, Update, Delete
+
 const users = ['Rafael', 'Lindoso', 'Araújo']
 
 //http://localhost:3000/users/20?nome=Lindoso
-server.get('/users/:index', (req, resp) => {
+server.get('/users', (req, res) => {
+  return res.json( users[index] );
+})
+
+server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
-  return resp.json({ message: `Hello ${users[index]}.` });
+  return res.json( users[index] );
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
 })
 
 server.listen(3000);
